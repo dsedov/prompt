0ebb61b0561b3dc6cbcc98cbaf2aa8ab9f16893c
dsedov <dennis.sedov@gmail.com>
2022-08-28 13:14:12 -0700

    fetch prompts

--- notion.py
+++ notion.py
@@ -9,6 +9,33 @@ class Notion:
         return    {"Authorization" : access_key,
                   "Content-Type" : "application/json",
                   "Notion-Version": "2022-02-22"}
+    def filtered_prompts(self, query):
+        prompts = []
+        
+        database_id = self.config["notion"]["prompt_db"]
+
+        headers = self.headers()
+        url = f"https://api.notion.com/v1/databases/{database_id}/query"
+        response = requests.post(url, headers = headers, data = json.dumps(query))
+        if response.status_code == 200:
+            data = json.loads(response.content)
+            results = data["results"]
+            if data["has_more"]:
+                while data["has_more"]:
+                    sleep(0.3)
+                    query["start_cursor"] = data["next_cursor"]
+                    response = requests.post(url, headers = headers, data = json.dumps(query))
+                    if response.status_code == 200:
+                        data = json.loads(response.content)
+                        results = results + data["results"]
+                    else: break
+
+            for result in results:
+                prompt = {}
+                prompt["prompt"] = result["properties"]["prompt"]["title"][0]["plain_text"]
+                prompt["id"] = result["id"]
+                prompts.append(prompt)
+        return prompts
     def filtered_artists(self, query):
         artists = []
         
@@ -36,6 +63,19 @@ class Notion:
                 artist["id"] = result["id"]
                 artists.append(artist)
         return artists
+    def artist_study_prompts(self):
+        return self.filtered_prompts({
+                    "filter": {
+                        "property": "tags",
+                        "multi_select": {
+                            "contains": "artist_study"
+                        }
+                    },
+                    "sorts": [{
+                        "property": "prompt",
+                        "direction": "ascending"
+                    }]
+                 })
     def fav_artists(self):
         return self.filtered_artists({
                     "filter": {
@@ -62,7 +102,7 @@ class Notion:
                         "direction": "ascending"
                     }]
                  })
-n = Notion("config.yaml")
-artists = n.empty_coherance_artists()
-print(artists)
+#n = Notion("config.yaml")
+#prompts = n.artist_study_prompts()
+#print(prompts)
 
